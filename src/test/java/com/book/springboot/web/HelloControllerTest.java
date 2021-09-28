701803f1e60d8e24e8a588e4310585deffaabe4d
Jiyoung <janice10508@gmail.com>
2021-09-29 02:29:34 +0900

    refactor

--- src/test/java/com/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/book/springboot/web/HelloControllerTest.java
@@ -11,19 +11,20 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@RunWith(SpringRunner.class)  //SpringRunner: 스프링 실행자 역할로, 스프링부트 테스트와 JUnit 사이를 연결해준다.
+@WebMvcTest(controllers = HelloController.class)  //Spring MVC에 집중하는 어노테이션.
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired  //스프링이 관리하는 Bean을 주입받음.
+    private MockMvc mvc;  //Spring MVC 테스트의 시작지점, GET과 POST에 대한 API테스트를 담당함.
 
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        mvc.perform(get("/hello"))  //hello 주소로 GET 요청.
+                //mvc.perform의 결과를 검증
+                .andExpect(status().isOk())  //HTTP Header의 Status를 검증.
+                .andExpect(content().string(hello));  //main에서 반환하는 문자열과 일치하는지 검증.
     }
 }
